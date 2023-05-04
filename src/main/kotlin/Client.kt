78f31547588246c453c2bd09d69261949f4e3e23
Sabina <abdiganieva.sb@phystech.edu>
2023-05-04 23:57:15 +0300

    fix #2: replaced lateinit with nullable types

--- src/main/kotlin/Client.kt
+++ src/main/kotlin/Client.kt
@@ -12,9 +12,10 @@ import java.net.Socket
  * @property receiver creates message receiver from server
  */
 class Client() : AutoCloseable {
-    private lateinit var socket: Socket
-    private lateinit var sender: PrintWriter
-    private lateinit var receiver: DataInputStream
+    private var socket: Socket? = null
+    private var sender: PrintWriter? = null
+    private var receiver: DataInputStream? = null
+
 
     /**
      * Creates connection to desired server destination
@@ -24,18 +25,18 @@ class Client() : AutoCloseable {
      */
     fun connect(address: String = "127.0.0.1", port: Int = 9999) {
         socket = Socket(address, port)
-        sender = PrintWriter(socket.getOutputStream(), true)
-        receiver = DataInputStream(socket.inputStream)
-        println("Successful connection : ${socket.inetAddress.hostAddress}.")
+        sender = PrintWriter(socket!!.getOutputStream(), true)
+        receiver = DataInputStream(socket!!.inputStream)
+        println("Successful connection : ${socket!!.inetAddress.hostAddress}.")
     }
 
     /**
      * Closes connection to the server
      */
     override fun close() {
-        socket.close()
-        receiver.close()
-        sender.close()
+        socket!!.close()
+        receiver!!.close()
+        sender!!.close()
         println("Connection ended by client.")
     }
 
@@ -45,25 +46,25 @@ class Client() : AutoCloseable {
      * @param message message to be sent
      */
     fun sendMessage(message: String = "") {
-        sender.println(message)
-        sender.flush()
-        println("Message $message was sent to the server ${socket.inetAddress.hostAddress}.")
+        sender!!.println(message)
+        sender!!.flush()
+        println("Message $message was sent to the server ${socket!!.inetAddress.hostAddress}.")
     }
 
     /**
      * Receives HELLO\n from server
      */
-    fun receiveHello() = println("Server replied: ${receiver.readNBytes(6).toString(Charsets.US_ASCII)}") // HELLO consists of 6 bytes
+    fun receiveHello() = println("Server replied: ${receiver?.readNBytes(6)!!.toString(Charsets.US_ASCII)}") // HELLO consists of 6 bytes
 
     /**
      * Receives OK<some garbage> from server until \n is reached
      */
     fun receiveOk() {
-        println("Server replied: ${receiver.readNBytes(2).toString(Charsets.US_ASCII)}") // Receive OK
-        var responseChar: Byte = receiver.readByte()
+        println("Server replied: ${receiver!!.readNBytes(2).toString(Charsets.US_ASCII)}") // Receive OK
+        var responseChar: Byte = receiver!!.readByte()
         while (responseChar.toInt() != '\n'.code) { // If server sends some garbage
             print(responseChar)
-            responseChar = receiver.readByte()
+            responseChar = receiver!!.readByte()
         }
     }
 
@@ -73,11 +74,11 @@ class Client() : AutoCloseable {
      * @return list with received bytes from RES response
      */
     fun receiveRes(): List<UByte> {
-        receiver.readNBytes(3) // Reads RES from server. No meaningful info here, so it's not assigned to anything
-        val msgLength = receiver.readByte().toInt() // Reads message length
-        val msg = receiver.readNBytes(msgLength).map { it.toUByte() }
-        receiver.readByte() // \n symbol encodes with 2 bytes so response from server contains extra zero, we read it here
-        if (receiver.readByte().toInt() != '\n'.code) println("""\n was not found in the end of message, something's wrong!""")
+        receiver!!.readNBytes(3) // Reads RES from server. No meaningful info here, so it's not assigned to anything
+        val msgLength = receiver!!.readByte().toInt() // Reads message length
+        val msg = receiver!!.readNBytes(msgLength).map { it.toUByte() }
+        receiver!!.readByte() // \n symbol encodes with 2 bytes so response from server contains extra zero, we read it here
+        if (receiver!!.readByte().toInt() != '\n'.code) println("""\n was not found in the end of message, something's wrong!""")
         return msg
     }
 }
